24369d1345d1792989bf896bbd6c168764f44d86
bannakonz <bannakon10@gmail.com>
2025-04-19 15:23:32 +0700

    chore: setup css

---
+++ src/module/ui/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <nav style={{background: 'green', height: '50px'}}>
+            <ul style={{margin: '0px', height: '100%',  padding: '0px 20px', listStyle: 'none', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <li>Logo</li>
+                <div style={{display: 'flex', columnGap: '20px',  justifyContent: 'space-between'}}>
+                    <li>Toggle</li>
+                    <li>Cart</li>
+                </div>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/module/ui/components/Layout.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header.jsx";
+
+export default function Layout() {
+    return (
+        <>
+            <Header></Header>
+            <div>Layout</div>
+        </>
+    )
+}
