65dfa599975db4973baae5a9a071f710db8fd3b1
bannakonz <bannakon10@gmail.com>
2025-04-22 07:52:41 +0700

    feat: add navigate to cart, and prevent effect from parent use stop-propagation

--- src/module/ui/components/Header.jsx
+++ src/module/ui/components/Header.jsx
@@ -1,13 +1,18 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import Icon from "../../util/Icon.jsx";
 
 export default function Header() {
+    const navigate = useNavigate();
+    const navigateToCart = () => {
+        navigate("/cart")
+    }
     return (
         <nav style={{background: 'green', height: '50px'}}>
             <ul style={{margin: '0px', height: '100%',  padding: '0px 20px', listStyle: 'none', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Link  to="">Logo</Link>
                 <div style={{display: 'flex', columnGap: '20px',  justifyContent: 'space-between'}}>
                     <li>Toggle</li>
-                    <li>Cart</li>
+                    <Icon type="cart" onClick={navigateToCart}>Cart</Icon>
                 </div>
             </ul>
         </nav>
--- src/module/util/Icon.jsx
+++ src/module/util/Icon.jsx
@@ -1,14 +1,16 @@
-export default function Icon({type = ''}) {
+export default function Icon({type = '', onClick }) {
     const iconPath = (iconType) => {
         if (iconType === 'bin') {
-            return 'src/assets/bin.svg'
+            return 'src/assets/bin.svg';
+        } else if (iconType === "cart") {
+            return 'src/assets/cart-shopping.svg'
         } else {
             return 'src/assets/headphone.svg';
         }
     }
     return (
         <>
-            <img src={iconPath(type)} alt={'icon'} width={'24px'} height={'24px'} style={{alignSelf: 'center'}}/>
+            <img onClick={onClick} src={iconPath(type)} alt={'icon'} width={'24px'} height={'24px'} style={{alignSelf: 'center', cursor: 'pointer'}}/>
         </>
     )
 }
