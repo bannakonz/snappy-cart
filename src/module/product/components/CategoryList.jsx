500b7b2eca5a5c9ea157fb58557cbb80d2d39331
bannakonz <bannakon10@gmail.com>
2025-04-27 14:08:59 +0700

    feat: filter product by param

--- src/module/product/components/CategoryList.jsx
+++ src/module/product/components/CategoryList.jsx
@@ -1,8 +1,10 @@
 import CategoryItem from "./CategoryItem.jsx";
+import {useLocation} from "react-router-dom";
 
-export default function CategoryList({handleSearchProduct}){
+export default function CategoryList(){
 
     const CATEGORY = [
+        { id: 0, title: 'all'},
         { id: 1, title: 'electronics' },
         { id: 2, title: 'audio' },
         { id: 3, title: 'fashion' },
@@ -13,7 +15,7 @@ export default function CategoryList({handleSearchProduct}){
     return (
         <div style={{display: 'flex', justifyContent: 'space-around'}}>
             {CATEGORY?.map((category)=>{
-                return <CategoryItem key={category.id} style={{color: 'red'}} category={category} handleSearchProduct={handleSearchProduct}/>
+                return <CategoryItem key={category.id} style={{color: 'red'}} category={category}/>
             })}
         </div>
     )
