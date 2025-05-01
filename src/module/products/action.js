f6aeef7332e111495b0f868a6d1ad12b18ba77b9
bannakonz <bannakon10@gmail.com>
2025-05-01 17:07:18 +0700

    feat: load product detail redux

--- src/module/products/action.js
+++ src/module/products/action.js
@@ -1,6 +1,9 @@
 const LOAD_PRODUCTS_REQUEST = 'app/products/LOAD_PRODUCTS_REQUEST';
 const LOAD_PRODUCTS_SUCCESS = 'app/products/LOAD_PRODUCTS_SUCCESS';
 const LOAD_PRODUCTS_FAILURE = 'app/products/LOAD_PRODUCTS_FAILURE';
+const LOAD_PRODUCT_DETAIL_REQUEST = 'app/product/LOAD_PRODUCT_DETAIL_REQUEST';
+const LOAD_PRODUCT_DETAIL_SUCCESS = 'app/product/LOAD_PRODUCT_DETAIL_SUCCESS';
+const LOAD_PRODUCT_DETAIL_FAILURE = 'app/product/LOAD_PRODUCT_DETAIL_FAILURE';
 
 
 function loadProducts(category) {
@@ -32,9 +35,37 @@ function loadProducts(category) {
     }
 }
 
+
+function loadProductDetail(id) {
+    console.log('id load = ', id);
+    return async (dispatch) => {
+        dispatch({type: LOAD_PRODUCT_DETAIL_REQUEST})
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BASE_URL}/products/${id}`)
+            if (!response.ok) {
+                console.log('load product detail some thing went wrong');
+            }
+            const productDetailData = await response.json();
+            dispatch({
+                type: LOAD_PRODUCT_DETAIL_SUCCESS,
+                payload: {
+                    product: productDetailData
+                }
+            })
+        } catch (err) {
+            console.log(`load product detail failure ${err}`);
+            dispatch({type: LOAD_PRODUCT_DETAIL_FAILURE})
+        }
+    }
+}
+
 export {
     LOAD_PRODUCTS_REQUEST,
     LOAD_PRODUCTS_SUCCESS,
     LOAD_PRODUCTS_FAILURE,
-    loadProducts
+    loadProducts,
+    LOAD_PRODUCT_DETAIL_REQUEST,
+    LOAD_PRODUCT_DETAIL_SUCCESS,
+    LOAD_PRODUCT_DETAIL_FAILURE,
+    loadProductDetail,
 }
